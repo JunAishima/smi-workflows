e8e94506a8b5528e829d56559ffca64dce5cf270
Jun Aishima <jaishima@bnl.gov>
2026-02-13 11:29:52 -0500

    run tasks concurrently

--- data_validation.py
+++ data_validation.py
@@ -17,7 +17,12 @@ def check_stream(run):
         logger.info(f"{stream} nbytes = {stream_data.nbytes:_}")
 
 
-@flow(retries=2, retry_delay_seconds=10)
+@task(retries=2, retry_delay_seconds=10)
+def validate_local(run_client):
+    logger = get_run_logger()
+    validate(run_client, fix_errors=True, try_reading=True, raise_on_error=True)
+
+@flow
 def data_validation(uid, beamline_acronym="smi"):
     logger = get_run_logger()
     api_key = Secret.load("tiled-smi-api-key", _sync=True).get()
@@ -26,8 +31,8 @@ def data_validation(uid, beamline_acronym="smi"):
     run_client_raw = tiled_client[beamline_acronym]["raw"][uid]
     logger.info(f"Launching tasks to check streams and validate uid {uid}")
     start_time = ttime.monotonic()
-    check_stream_task = check_stream.submit(run)
-    validate_task = validate.submit(run_client, fix_errors=True, try_reading=True, raise_on_error=True)
+    check_stream_task = check_stream.submit(run_client_raw)
+    validate_task = validate_local.submit(run_client)
     logger.info("Waiting for tasks to complete")
     check_stream_task.result()
     validate_task.result()
